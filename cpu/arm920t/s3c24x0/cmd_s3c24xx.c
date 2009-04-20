cd13b0bbc4c91f7954732f04953f7de5e9b5646e
Harald Welte <laforge@gnumonks.org>
2009-04-21 01:39:54 +0200

    [ARM] S3C24xx: Add GPIO related commands
    This patch introduces new 's3c24xx gpio get' and 's3c24xx gpio show' commands
    to inspect and modify the GPIO configuration.
    
    Signed-off-by: Harald Welte <laforge@gnumonks.org>

--- cpu/arm920t/s3c24x0/cmd_s3c24xx.c
+++ cpu/arm920t/s3c24x0/cmd_s3c24xx.c
@@ -1,6 +1,7 @@
 /*
- * (C) Copyright 2006 by OpenMoko, Inc.
- * Author: Harald Welte <laforge@openmoko.org>
+ * (C) Copyright 2006-2008 by OpenMoko, Inc.
+ * Authors: Harald Welte <laforge@openmoko.org>
+ *	    Werner Almesberger <werner@openmoko.org>
  *
  * See file CREDITS for list of people who contributed to this
  * project.
@@ -242,6 +243,272 @@ static int reconfig_mpll(u_int16_t mhz)
 	return -1;
 }
 
+/* GPIO handling, taken from Openmoko SVN trunk/src/target/gpio/gpio.c */
+
+#define BASE 0x56000000
+
+static volatile void *mem = (void *)BASE;
+
+#define CON_IN	0
+#define CON_OUT	1
+#define CON_F1	2
+#define CON_F2	3
+
+static struct port {
+	const char *name;
+	int offset;
+	int last;
+	enum { pt_a, pt_b } type;
+} ports[] = {
+	{ "A", 0x00, 22, pt_a },
+	{ "B", 0x10, 10, pt_b },
+	{ "C", 0x20, 15, pt_b },
+	{ "D", 0x30, 15, pt_b },
+	{ "E", 0x40, 15, pt_b },
+	{ "F", 0x50,  7, pt_b },
+	{ "G", 0x60, 15, pt_b },
+	{ "H", 0x70, 10, pt_b },
+	{ "J", 0xd0, 12, pt_b },
+	{ NULL, }
+};
+
+static void print_n(const char *name, int last)
+{
+	int i;
+
+	printf("%s ", name);
+	for (i = 0; i <= last; i++)
+		printf("%2d ", i);
+	putc('\n');
+}
+
+/* ----- Read a pin -------------------------------------------------------- */
+
+static const char *pin_a(int con, int dat)
+{
+	if (con)
+		return dat ? "F1" : "F0";
+	else
+		return dat ? ">1" : ">0";
+}
+
+
+static const char *pin_b(int con, int dat, int pud)
+{
+	static char res[4];
+
+	res[0] = " >FX"[con];
+	res[1] = dat ? '1' : '0';
+	res[2] = pud ? ' ' : 'R';
+	return res;
+}
+
+
+static const char *pin(const struct port *p, int num)
+{
+	uint32_t con, dat, pud;
+
+	con = *(uint32_t *) (mem+p->offset);
+	dat = *(uint32_t *) (mem+p->offset+4);
+	if (p->type == pt_a)
+		return pin_a((con >> num) & 1, (dat >> num) & 1);
+	else {
+		pud = *(uint32_t *) (mem+p->offset+8);
+		return pin_b((con >> (num*2)) & 3, (dat >> num) & 1,
+		    (pud >> num) & 1);
+	}
+}
+
+
+/* ----- Set a pin --------------------------------------------------------- */
+
+
+static void set_a(const struct port *p, int num, int c, int d)
+{
+	uint32_t con, dat;
+
+	con = *(uint32_t *) (mem+p->offset);
+	con = (con & ~(1 << num)) | ((c == CON_F1) << num);
+	*(uint32_t *) (mem+p->offset) = con;
+	
+	if (d != -1) {
+		dat = *(uint32_t *) (mem+p->offset+4);
+		dat = (dat & ~(1 << num)) | (d << num);
+		*(uint32_t *) (mem+p->offset+4) = dat;
+	}
+}
+
+
+static void set_b(const struct port *p, int num, int c, int d, int r)
+{
+	uint32_t con, dat, pud;
+
+	con = *(uint32_t *) (mem+p->offset);
+	con = (con & ~(3 << (num*2))) | (c << (num*2));
+	*(uint32_t *) (mem+p->offset) = con;
+	
+	if (d != -1) {
+		dat = *(uint32_t *) (mem+p->offset+4);
+		dat = (dat & ~(1 << num)) | (d << num);
+		*(uint32_t *) (mem+p->offset+4) = dat;
+	}
+
+	pud = *(uint32_t *) (mem+p->offset+8);
+	pud = (pud & ~(1 << num)) | (!r << num);
+	*(uint32_t *) (mem+p->offset+8) = pud;
+}
+
+
+static int set_pin(const struct port *p, int num, int c, int d, int r)
+{
+	if (num > p->last) {
+		printf("invalid pin %s%d\n", p->name, num);
+		return -1;
+	}
+	if (p->type == pt_a) {
+		if (r) {
+			printf("pin %s%d has no pull-down\n", p->name, num);
+			return -1;
+		}
+		if (c == CON_IN) {
+			printf("pin %s%d cannot be an input\n", p->name, num);
+			return -1;
+		}
+		if (c == CON_F2) {
+			printf("pin %s%d has no second function\n",
+				p->name, num);
+			return -1;
+		}
+		set_a(p, num, c, d);
+	}
+	else
+		set_b(p, num, c, d, r);
+
+	return 0;
+}
+
+static size_t strcspn(const char *s, const char *reject)
+{
+	int len = strlen(s);
+	int rej_len = strlen(reject);
+	int i, j;
+
+	for (i = 0; i < len; i++) {
+		for (j = 0; j < rej_len; j++) {
+			if (s[i] == reject[j])
+				return i;
+		}
+	}
+	return len;
+}
+
+static int port_op(const char *name, const char *op)
+{
+	const char *eq, *s;
+	const struct port *p;
+	int num, c, d = -1, r = 0;
+	char *end;
+
+	eq = strchr(op, '=');
+	if (!eq)
+		eq = strchr(op, 0);
+	num = strcspn(op, "0123456789");
+	if (!num || op+num >= eq)
+		return -1;
+	for (p = ports; p->name; p++)
+		if (strlen(p->name) == num && !strncmp(p->name, op, num))
+			break;
+	if (!p->name) {
+		printf("invalid port \"%.*s\"\n", num, op);
+		return -1;
+	}
+	num = simple_strtoul(op+num, &end, 10);
+	if (end != eq)
+		return -1;
+	if (!*eq) {
+		s = pin(p, num);
+		if (*s == ' ')
+			s++;
+		printf("%s\n", s);
+		return 0;
+	}
+	switch (eq[1]) {
+	case '0':
+		d = 0;
+		c = CON_OUT;
+		break;
+	case '1':
+		d = 1;
+		c = CON_OUT;
+		break;
+	case 'Z':
+	case 'z':
+		c = CON_IN;
+		break;
+	case 'R':
+	case 'r':
+		if (eq[2])
+			return -1;
+		c = CON_IN;
+		r = 1;
+		break;
+	case 'F':
+	case 'f':
+		c = CON_F1;
+		break;
+	case 'X':
+	case 'x':
+		c = CON_F2;
+		break;
+	default:
+		return -1;
+	}
+	if (eq[2]) {
+		if (eq[2] != 'R' && eq[2] != 'r')
+			return -1;
+		if (eq[3])
+			return -1;
+		r = 1;
+	}
+	return set_pin(p, num, c, d, r);
+}
+
+
+
+/* ----- Dump all ports ---------------------------------------------------- */
+
+static void dump_all(void)
+{
+	const struct port *p;
+	uint32_t con, dat, pud;
+	int i;
+
+	for (p = ports; p->name; p++) {
+		con = *(uint32_t *) (mem+p->offset);
+		dat = *(uint32_t *) (mem+p->offset+4);
+		if (p->type == pt_a) {
+			print_n(p->name, p->last);
+			printf("%*s ", strlen(p->name), "");
+			for (i = 0; i <= p->last; i++)
+				printf("%s ",
+				    pin_a((con >> i) & 1, (dat >> i) & 1));
+			putc('\n');
+		}
+		else {
+			pud = *(uint32_t *) (mem+p->offset+8);
+			print_n(p->name, p->last);
+			printf("%*s ", strlen(p->name), "");
+			for (i = 0; i <= p->last; i++)
+				printf("%s",
+				    pin_b((con >> (i*2)) & 3, (dat >> i) & 1,
+				    (pud >> i) & 1));
+			putc('\n');
+		}
+	}
+}
+
+
+
 int do_s3c24xx ( cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
 	if (!strcmp(argv[1], "speed")) {
@@ -264,6 +531,23 @@ int do_s3c24xx ( cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 				print_cpu_speed();
 		} else
 			goto out_help;
+	} else if (!strcmp(argv[1], "gpio")) {
+		if (argc < 2)
+			goto out_help;
+		else if (!strcmp(argv[2], "show"))
+			dump_all();
+		else if (!strcmp(argv[2], "set")) {
+			int i;
+			if (argc < 3)
+				goto out_help;
+			for (i = 3; i < argc; i++) {
+				int rc;
+				rc = port_op("s3c24xx-gpio", argv[i]);
+				if (rc < 0)
+					goto out_help;
+			}
+		} else
+			goto out_help;
 	} else {
 out_help:
 		printf("Usage:\n%s\n", cmdtp->usage);
@@ -282,6 +566,8 @@ U_BOOT_CMD(
 	"speed get - display current PLL speed config\n"
 	"s3c24xx speed list - display supporte PLL speed configs\n"
 	"s3c24xx speed set - set PLL speed\n"
+	"s3c24xx gpio show - show current GPIO configuration\n"
+	"s3c24xx gpio set - set one GPIO\n"
 );
 
 #endif
